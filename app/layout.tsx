ca672a642519e46d1e38aeea27dd07dc06a8e16b
ShalemValavala <valavalashalem@gmail.com>
2026-04-23 15:59:36 +0530

    fixed layout type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
+import React from "react";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-})  {
+}) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
       </head>
